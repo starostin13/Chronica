fdb20d07150189f1f23c1509f8e64cbb7ce156fb
starostin13 <starostin13@outlook.com>
2023-10-30 18:39:38 +0300

    Fix several bugs

--- bot.py
+++ bot.py
@@ -17,12 +17,13 @@ dst = '/temp/'
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     print("Answer from inline is " + call.data)
+    if call.data == "new":
+        newFolderName = createFolder()
     for entry in os.listdir(dst):
         if os.path.isfile(os.path.join(dst, entry)):
             if call.data == "new":
-                newFolderName = createFolder()
                 saveFileTo(os.path.join(dst, entry), newFolderName + "/" + entry)
-            if call.data != "decline":
+            elif call.data != "decline":
                 saveFileTo(os.path.join(dst, entry), call.data + "/" + entry)
             os.remove(os.path.join(dst, entry))
 
@@ -48,11 +49,39 @@ def send_welcome(message):
                 bot.send_video(credentials.chat_id, photo.file, caption = comment)
         
     except Exception as exc:
-        bot.send_message(credentials.chat_id, f"Unexpected {exc=}")
+        bot.send_message(credentials.chat_id, "Try to send from " + photo_path_splited[len(photo_path_splited) - 2] + "sUnexpected " + exc.description)
 
+
+@bot.message_handler(content_types=['video'])
+def echo_video(message):
+    try:
+        file = bot.get_file_url(message.video.file_id)
+        uploadedFileName = message.caption if message.caption != None else file.split('/')[-1]
+        print("Saving video " + uploadedFileName + " locally")
+            # writing to a custom file
+        with requests.get(file, stream=True) as r:
+            with open(dst + uploadedFileName, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+        bot.reply_to(message, "Мне это сохранить что ли?")
+    
+        lastUpdatedFolder = getLastUpdatedFolder()
+
+        markup = InlineKeyboardMarkup()
+        markup.row_width = 2
+        markup.add(InlineKeyboardButton(lastUpdatedFolder, callback_data=lastUpdatedFolder),
+                                   InlineKeyboardButton("Не нада", callback_data="decline"),
+                                   InlineKeyboardButton("Новое", callback_data="new"))
+
+        bot.reply_to(message, text="Куда сохранять-то?", reply_markup=markup)
+
+        for entry in os.listdir(dst):
+            if os.path.isfile(os.path.join(dst, entry)):
+                print(entry)
+    except Exception as exc:
+        bot.reply_to(message, "Не вышло: " + exc.description)
 
 @bot.message_handler(content_types=['image', 'photo'])
-def echo_all(message):
+def echo_photo(message):
     file = bot.get_file_url(message.photo[-1].file_id)    
     uploadedFileName = message.caption if message.caption != None else file.split('/')[-1]
     print("Saving photo " + uploadedFileName + " locally")
