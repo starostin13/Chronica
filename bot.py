d0f5fc37337e97aa44f339281c6f17e73bfbd35b
starostin13 <starostin13@outlook.com>
2023-11-01 21:48:53 +0300

    little bit refactoring

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from yaHelper import createFolder, downloadFile, getLastUpdatedFolder, getPhoto,
 from stringHelper import numberToMonthNameRu
 import shutil
 
-bot_token = credentials.bot_token
+bot_token = credentials.bot_tokenq
 bot = telebot.TeleBot(bot_token)
 dst = '/temp/'
 schedule = sched.scheduler(time.time, time.sleep)
@@ -52,8 +52,8 @@ def send_welcome(message):
         if photo.media_type == "video":
             if "gp3" in photo.name or "mp4" in photo.name:
                 downloadFile(photo.file, photo.name)
-                bot.send_video(credentials.chat_id, open(dst + '/' + photo.name, 'rb'), caption = comment)
-                os.remove(dst + '/' + photo.name)
+                bot.send_video(credentials.chat_id, open(dst + photo.name, 'rb'), caption = comment)
+                os.remove(dst + photo.name)
             else:
                 bot.send_video(credentials.chat_id, photo.file, caption = comment)
 
@@ -65,57 +65,19 @@ def send_welcome(message):
 def echo_video(message):
     try:
         file = bot.get_file_url(message.video.file_id)
-        uploadedFileName = message.caption if message.caption != None else file.split('/')[-1]
-        print("Saving video " + uploadedFileName + " locally")
-            # writing to a custom file
-        with requests.get(file, stream=True) as r:
-            with open(dst + uploadedFileName, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-        bot.reply_to(message, "Мне это сохранить что ли?")
-
-        lastUpdatedFolder = getLastUpdatedFolder()
-
-        markup = InlineKeyboardMarkup()
-        markup.row_width = 2
-        markup.add(InlineKeyboardButton(lastUpdatedFolder, callback_data=lastUpdatedFolder),
-                                   InlineKeyboardButton("Не нада", callback_data="decline"),
-                                   InlineKeyboardButton("Новое", callback_data="new"))
-
-        bot.reply_to(message, text="Куда сохранять-то?", reply_markup=markup)
-
-        for entry in os.listdir(dst):
-            if os.path.isfile(os.path.join(dst, entry)):
-                print(entry)
+        recievingFile(file, message)
+        
     except Exception as exc:
         bot.reply_to(message, "Не вышло: " + exc.description)
 
 @bot.message_handler(content_types=['image', 'photo'])
 def echo_photo(message):
-    file = bot.get_file_url(message.photo[-1].file_id)
-    uploadedFileName = message.caption if message.caption != None else file.split('/')[-1]
-    print("Saving photo " + uploadedFileName + " locally")
+    try:
+        file = bot.get_file_url(message.photo[-1].file_id)
+        recievingFile(file, message)
 
-    # writing to a custom file
-    with requests.get(file, stream=True) as r:
-        with open(dst + uploadedFileName, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
-
-    bot.reply_to(message, "Мне это сохранить что ли?")
-
-    lastUpdatedFolder = getLastUpdatedFolder()
-
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 2
-    markup.add(InlineKeyboardButton(lastUpdatedFolder, callback_data=lastUpdatedFolder),
-                               InlineKeyboardButton("Не нада", callback_data="decline"),
-                               InlineKeyboardButton("Новое", callback_data="new"))
-
-    bot.reply_to(message, text="Куда сохранять-то?", reply_markup=markup)
-
-    for entry in os.listdir(dst):
-        if os.path.isfile(os.path.join(dst, entry)):
-            print(entry)
-
+    except Exception as exc:
+        bot.reply_to(message, "Не вышло: " + exc.description)
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
@@ -141,6 +103,32 @@ def main_loop():
     
     bot.infinity_polling()
 
+
+def recievingFile(file, message):    
+    uploadedFileName = message.caption if message.caption != None else file.split('/')[-1]
+    
+    print("Saving file " + uploadedFileName + " locally")
+        # writing to a custom file
+    with requests.get(file, stream=True) as r:
+        with open(dst + uploadedFileName, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+            
+    bot.reply_to(message, "Мне это сохранить что ли?")
+
+    lastUpdatedFolder = getLastUpdatedFolder()
+
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton(lastUpdatedFolder, callback_data=lastUpdatedFolder),
+                                InlineKeyboardButton("Не нада", callback_data="decline"),
+                                InlineKeyboardButton("Новое", callback_data="new"))
+
+    bot.reply_to(message, text="Куда сохранять-то?", reply_markup=markup)
+
+    for entry in os.listdir(dst):
+        if os.path.isfile(os.path.join(dst, entry)):
+            print(entry)
+    
 
 def dump_prin():
     send_welcome("dump message")
